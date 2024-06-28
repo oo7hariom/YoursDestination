4330fb2a1c1d6ad6d7af6c8fd2f1f8b3bd998152
hariom <154430401+oo7hariom@users.noreply.github.com>
2024-06-28 18:05:41 +0530

    change in model when listing deleted the review will be deleted from database

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,6 @@
 const { ref } = require("joi");
 const mongoose = require("mongoose");
-
+const Review = require("./review.js");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -31,6 +31,12 @@ const listingSchema = new Schema({
     }
   ]
     
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
