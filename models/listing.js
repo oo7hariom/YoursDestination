1e0ea064ae63707e5742d4d87c5f169a2c623f8c
hariom <154430401+oo7hariom@users.noreply.github.com>
2024-06-28 14:53:59 +0530

    reviw model created and relation to each listing aaplied

--- models/listing.js
+++ models/listing.js
@@ -1,3 +1,4 @@
+const { ref } = require("joi");
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -18,11 +19,18 @@ const listingSchema = new Schema({
       v === ""
         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
         : v,
-  },
+  }, 
 
     price: Number,
     location: String,
-    country:String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref : "Review",
+    }
+  ]
+    
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
