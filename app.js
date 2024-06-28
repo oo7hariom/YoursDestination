95d38136a211a161581a4aad83d62debf4eacee3
hariom <154430401+oo7hariom@users.noreply.github.com>
2024-06-28 17:55:10 +0530

    delete review feature implemented

--- app.js
+++ app.js
@@ -117,18 +117,23 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 //review route
-
 app.post("/listings/:id/reviews",validateReview, wrapAsync(async (req, res) => {
-   
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-
     listing.reviews.push(newReview);
-
     await newReview.save();
     await listing.save();
-    
     res.redirect(`/listings/${listing._id}`);
+}));
+
+//delete review route
+
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });       
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
 
 
 }));
