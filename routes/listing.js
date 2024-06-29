5e6571886dea3cc21661f675f427a315780f54fe
hariom <154430401+oo7hariom@users.noreply.github.com>
2024-06-29 12:10:36 +0530

    route fixes

--- routes/listing.js
+++ routes/listing.js
@@ -75,4 +75,4 @@ router.post("/",validateListing, wrapAsync(async (req, res) => {
 
 //----------------------------*****************************-------------------------------
 
-module.exports = router;
+module.exports = router; 
