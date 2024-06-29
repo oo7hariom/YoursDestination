14a6ef0a88e9159601b9266f8618a0959ce42183
hariom <154430401+oo7hariom@users.noreply.github.com>
2024-06-29 09:26:28 +0530

    review routes reStructured

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/YoursDestination";
-const { listingSchema } = require("./schema.js");
-const { reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
 const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
+
+//------------------------------------***************************************-----------------------------------------
 main()
     .then(() => {
         console.log("connected to DataBase");
@@ -25,6 +23,10 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+
+
+//-----------------------------------*************************************************----------------------------------------
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -32,64 +34,22 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-// function validateListing(req, res, next) {
-//     const { listing } = req.body;
-//     if (!listing.title || !listing.price || !listing.location || !listing.country) {
-//         throw new ExpressError(400, "All fields are required");
-//     }
-//     if (isNaN(listing.price)) {
-//         throw new ExpressError(400, "Price must be a number");
-//     }
-//     next();
-// }
 
-
-
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-   
-    if (error) {
-        throw new ExpressError(400, error);
-    }
-    else
-        next();
-};
-
-
-
-
+//------------ACQURING THE LISTING AND REVIEW ROUTES--------------------------
 app.use("/listings", listings);
+app.use("/listings/:id/reviews",reviews);
+//-------------****************************----------------------------------
 
 
-
-
-//review route
-app.post("/listings/:id/reviews",validateReview, wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-//delete review route
-
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });       
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-
-
-}));
-
-
+//---------------INDEX ROUTE-----------------------------------------------------
 app.get("/", (req, res) => {
     res.send("You are at the main page");
 });
+//-----------------**********************************-------------------------------------
 
+
+
+//-------------------ERROR HANDLING MIDDLEWARES-----------------------------------------------
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page not found"));
 });
@@ -103,3 +63,4 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
 });
+//------------------------*********************---------------------------------------
