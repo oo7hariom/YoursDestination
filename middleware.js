b006257dd9efd671cd419f7f75c3e1cb6abef098
hariom <154430401+oo7hariom@users.noreply.github.com>
2024-07-01 16:44:12 +0530

    resturctured all middleware at one place

--- middleware.js
+++ middleware.js
@@ -1,5 +1,9 @@
- 
- 
+const Listing = require("./models/listing");
+ const { listingSchema,reviewSchema } = require("./schema.js");
+const ExpressError = require("./utils/ExpressError.js");
+
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     // console.log(req.path , "...", req.originalUrl);
    
@@ -8,9 +12,9 @@ module.exports.isLoggedIn = (req, res, next) => {
         req.session.redirectUrl = req.originalUrl;
         req.flash("error", "First login to do furthur action");
         return res.redirect("/login");
-    } 
+    }
     next();
-}
+};
 
 
 module.exports.saveRedirectUrl = (req, res, next) => {
@@ -18,4 +22,35 @@ module.exports.saveRedirectUrl = (req, res, next) => {
         res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
-}
+};
+
+module.exports.isOwner =async (req,res,next) => {
+       const { id } = req.params;
+    let listing = await Listing.findById(id);
+    if (!listing.owner._id.equals(res.locals.currUser._id)) {
+        req.flash("error", "you are not the Owner");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
+
+ module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+   
+    if (error) {
+        throw new ExpressError(400, error);
+    }
+    else
+        next();
+};
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+   
+    if (error) {
+        throw new ExpressError(400, error);
+    }
+    else
+        next();
+};
+
